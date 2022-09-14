be0a8b5f19a445a024ca0f21583f192cafc38a32
William Souza <wgui@live.com>
2022-09-14 18:48:39 -0300

    font config

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
       title: 'Personal Expenses',
       theme: ThemeData(
         primarySwatch: Colors.teal,
+        fontFamily: 'Quicksand',
       ),
       home: const MyHomePage(),
     );
